00354d3c90b13b92b7be85583c9f7e027a12040c
Vasanth <vasanthakumarvk677429@gmail.com>
2024-05-31 02:18:43 +0530

    update about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import AboutImage from "../assets/vk-about.png";
 
 const About = () => {
   const config ={
-    line1: 'Aspiring to be a full-stack developer, I am currently diving deep into React.js, Next.js, and MongoDB, building on my foundation in HTML5, CSS3, JavaScript, and Tailwind CSS. My journey is fueled by a passion for creating dynamic and responsive web applications. With each project, I am honing my skills and getting closer to achieving my goal.',
+    line1: 'Aspiring to be a full-stack developer, I am currently diving deep into React.js, Node.js, Express.js and MongoDB, building on my foundation in HTML5, CSS3, JavaScript, and Tailwind CSS. My journey is fueled by a passion for creating dynamic and responsive web applications. With each project, I am honing my skills and getting closer to achieving my goal.',
   }
   return (
     <section id="about" className="flex flex-col md:flex-row px-5 py-20 bg-secondary justify-between">
